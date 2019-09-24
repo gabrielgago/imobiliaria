994dfc3ac4f155601fe0fa8d528c024a33f96d6d
Gabriel Martins <gfrael@gmail.com>
2019-09-24 15:44:09 -0300

    alterações

--- Imobiliaria/src/main/java/br/com/contadores/config/AppConfig.java
+++ Imobiliaria/src/main/java/br/com/contadores/config/AppConfig.java
@@ -1,8 +1,40 @@
 package br.com.contadores.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
-public class AppConfig {
- 
+@EnableWebMvc
+@ComponentScan(basePackages = { "br.com.contadores" })
+public class AppConfig extends WebMvcConfigurerAdapter {
+
+	private static final String[] CLASSPATH_RESOURCE_LOCATIONS = { "classpath:/META-INF/resources/",
+			"classpath:/resources/", "classpath:/static/", "classpath:/public/" };
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/pages/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry
+        .addResourceHandler("/resources/**")
+        .addResourceLocations("/resources/"); 
+	}
+
 }
