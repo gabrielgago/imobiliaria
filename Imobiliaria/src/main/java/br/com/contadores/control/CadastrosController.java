5a7670edc429551f11288aac5abc75cd35fbe45a
Gabriel Martins <gfrael@gmail.com>
2019-10-24 15:07:01 -0300

    jj
    jj

--- Imobiliaria/src/main/java/br/com/contadores/control/CadastrosController.java
+++ Imobiliaria/src/main/java/br/com/contadores/control/CadastrosController.java
@@ -1,16 +1,13 @@
 package br.com.contadores.control;
 
-import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
